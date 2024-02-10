d525ea58ae02895e914ef35662112a4450e97f7a
lekefua <145169940+lekefua@users.noreply.github.com>
2024-02-10 02:11:36 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 /*
 terraform {
   cloud {
-    organization = "gcp_cloudamor_security_dev_evaristus"
+    organization = "LENS 1"
 
     workspaces {
       name = "cloud_armor_factory-9th-feb
@@ -11,10 +11,10 @@ terraform {
 }*/
 terraform {
   cloud {
-    organization = "gcp_cloudamor_security_dev_evaristus"
+    organization = "LENS 1"
 
     workspaces {
-      name = "cloud_armor_factory_mcit_8thFeb"
+      name = "gcp_cloudamor_security_dev_evaristus"
     }
   }
 }
